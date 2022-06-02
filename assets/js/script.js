b03ff4115f2f10b374a5c54f0c86a9a5f077818b
Saikat-Chowdhury90 <sonusaikat09@gmail.com>
2022-06-02 18:36:07 +0530

    animation added

--- assets/js/script.js
+++ assets/js/script.js
@@ -212,7 +212,23 @@ youtube defer
 window.addEventListener('load', ytdefer_setup);
 
 
-
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
 
 
 
